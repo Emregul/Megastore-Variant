b1df8ac6effe1ab1ae44992b48419dc9fa91faaa
Emre Gul <emreegul@gmail.com>
2013-03-12 20:06:43 -0700

    Fixed a compilation Bug

--- Megastore/src/Settings.java
+++ Megastore/src/Settings.java
@@ -6,13 +6,12 @@ import java.util.HashMap;
 public class Settings 
 {//Settings for IPs
 	public static HashMap<Integer,InetSocketAddress> serverIpMap = new HashMap<Integer,InetSocketAddress>();
-	public static InetSocketAddress[] serverIpList;
+	public static InetSocketAddress[] serverIpList = {new InetSocketAddress("localhost", 11111), new InetSocketAddress("localhost", 11112),new InetSocketAddress("localhost", 11113)};
 	
 	public static void init()
 	{
-		serverIpMap.put(1, new InetSocketAddress("localhost", 11115));
-		serverIpMap.put(2, new InetSocketAddress("127.0.0.1", 11112));
-		serverIpMap.put(3, new InetSocketAddress("127.0.0.1", 11113));
-		serverIpList = (InetSocketAddress[]) serverIpMap.values().toArray();
+		for (int i = 0; i < 3; i++) {
+			serverIpMap.put(i+1,serverIpList[i]);
+		}
 	}
 }
