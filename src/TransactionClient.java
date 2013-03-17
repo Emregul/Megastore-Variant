10348b8fdf603871fa206981d9f9f7f4eb37b35d
Emregul <emreegul@gmail.com>
2013-03-17 01:11:48 -0700

    Fixed a lot of bugs, needs more fixing.

---
+++ src/TransactionClient.java
@@ -0,0 +1,359 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.HashMap;
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import java.util.*;
+
+public class TransactionClient {
+	//maintains the state
+	private long propNum;
+	private HashMap<Long,Transaction> ActiveTransactions;
+	private Database db;
+	private long id;
+	
+	/**
+	 * Constructor
+	 */
+	public TransactionClient(long id) {
+		propNum = 0;
+		this.id = id;
+		ActiveTransactions = new HashMap<Long,Transaction>();
+		try {
+			db = new Database();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Receive Messages from AppLayer
+	 */
+	public void ReceiveBeginMessageFromAppLayer(long transactionID) {
+		this.begin(transactionID);
+	}
+
+	public String ReceiveReadMessageFromAppLayer(long transactionID, String key) {
+		String result = read(transactionID, key);
+		return result;
+	}
+	
+	public void ReceiveWriteMessageFromAppLayer(long transactionID, String key, String value) {
+		write(transactionID, key, value);
+	}
+	
+	public void ReceiveCommitMessageFromAppLayer(long transactionID) {
+		commit(transactionID);
+	}
+	
+	public void ReceiveAbortMessageFromAppLayer(long transactionID) {
+		abort(transactionID);
+	}
+	
+	/**
+	 * SendMessagetoServerX
+	 * @param serverAddress
+	 * @param message
+	 * @return
+	 */
+	public String SendMessagetoServerX(InetSocketAddress serverAddress, String message)
+	{//Send a string message to a given server
+		Socket socket;
+		try {
+			socket = new Socket(serverAddress.getHostName(),serverAddress.getPort());
+			MessageSender newThread = new MessageSender(socket,message);
+			newThread.start();
+			while (newThread.isAlive())
+			{
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			return newThread.parsedMessage;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public class MessageSender extends Thread
+	{//Thread that opens the socket connection and sends the message
+		private Socket socket;
+		private PrintWriter toTransactionServer;
+		private BufferedReader fromTransactionServer;
+		private String message;
+		private String parsedMessage;
+		
+		public MessageSender(Socket socket, String message)
+		{
+			this.socket = socket;
+			this.message = message;
+			
+			try {
+				toTransactionServer = new PrintWriter (new OutputStreamWriter(socket.getOutputStream()));
+				fromTransactionServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		public void run()
+		{
+			try {
+				//Parse given message send it to the server, read response from server and close the socket
+				toTransactionServer.println(message);
+				toTransactionServer.flush();
+				parsedMessage = fromTransactionServer.readLine();
+				socket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    
+		}
+	}
+	
+
+	/**
+	 * begin - a transaction with given transactionID
+	 * @param transactionID
+	 */
+	public void begin(long transactionID) {
+		Transaction t = new Transaction(transactionID);
+		ActiveTransactions.put(transactionID,t);
+	}
+	
+	
+	/**
+	 * read - key for transaction represented by transactionID
+	 * @param transactionID
+	 * @param key
+	 * @return
+	 */
+	public String read(long transactionID, String key) {
+
+		Transaction t = ActiveTransactions.get(transactionID);
+		String value = t.readLocal(key);
+		if(value != null)
+			return value;
+		else
+			try {
+				value = db.read(key, t.timestamp);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		return value;
+	}
+	
+	/**
+	 * write - write to local WriteSet of transaction - on commit - this WriteSet would be written to DataStore
+	 * @param transactionID
+	 * @param key
+	 * @param value
+	 */
+	public void write(long transactionID, String key, String value) {
+		Transaction t = ActiveTransactions.get(transactionID);
+		t.addToWriteSet(key,value);
+	}
+	
+	/**
+	 * commit - Transaction
+	 * @param transactionID
+	 * @return true - on success , false - on failure
+	 */
+	public boolean commit(long transactionID) {
+		Transaction t = ActiveTransactions.get(transactionID);
+		//code for commit protocol
+		HashMap<String,String> propVal = t.WriteSet;
+		
+		if(runPAXOSGivenPropNum(propVal)) {
+			ActiveTransactions.remove(transactionID);
+			return true;
+		}
+		//promotion here
+		else 
+			return false;
+	}
+	/**
+	 * abprt - Transaction
+	 * @param transactionID
+	 * @return
+	 */
+	public boolean abort(long transactionID)
+	{
+		//to be written
+		return true;
+	}
+	private boolean runPAXOSGivenPropNum(HashMap<String,String> propVal)
+	{
+		int D = Settings.serverIpList.length;
+		//PREPARE PHASE
+		List<MessageContent> responseSet = preparePhaseForPAXOS(propVal);
+		//ACCEPT PHASE
+		HashMap<String,String> propValue = findWinningVal(responseSet, propVal);
+		
+		int ackCount = 0;
+		for (int i = 0; i < Settings.serverIpList.length; i++) 
+		{
+			InternalMessageLog.WriteLog("client"+id, " sent " + Messages.sendAcceptFromClientToService(i+1, propNum, propValue));
+			String response = SendMessagetoServerX(Settings.serverIpList[i],Messages.sendAcceptFromClientToService(i+1, propNum, propValue));			
+			MessageContent parsedResponse = Messages.parse(response);
+			InternalMessageLog.WriteLog("client "+id, " received " + parsedResponse.toString());
+			System.out.println(parsedResponse.status);
+			if (parsedResponse.status)
+				ackCount++;
+		}
+		
+		if (ackCount < D/2)
+		{
+			try {
+				Thread.sleep((long) (Math.random()*100));
+				propNum  = nextPropNumber(responseSet,propNum+1);
+				InternalMessageLog.WriteLog("client"+id, "FAILED AT ACCEPT, RESTARTING WITH PROPNUM " +  propNum);
+				return runPAXOSGivenPropNum(propVal);
+				//return false;
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		for (int i = 0; i < Settings.serverIpList.length; i++) 
+		{
+			InternalMessageLog.WriteLog("client"+id, " sent " + Messages.sendApplyFromClientToService(i+1, propNum, propValue));
+			SendMessagetoServerX(Settings.serverIpList[i], Messages.sendApplyFromClientToService(i+1, propNum, propValue));	
+		}
+		if (propValue.equals(propVal))
+		{
+			InternalMessageLog.WriteLog("client"+id, "COMMITTED :) " +propValue);
+			return true;
+		}else
+		{
+			InternalMessageLog.WriteLog("client"+id, "ABORTED :(");
+			return false;
+		}
+		
+		
+	}
+	
+	private List<MessageContent>  preparePhaseForPAXOS(HashMap<String,String> propVal)
+	{
+		boolean keepTrying = true;
+		int D = Settings.serverIpList.length;
+		List<MessageContent> responseSet = null;
+		int ackCount;
+		while (keepTrying)
+		{
+			ackCount = 0;
+			responseSet = new ArrayList<MessageContent>();
+			for (int i = 0; i < Settings.serverIpList.length; i++) 
+			{
+				InternalMessageLog.WriteLog("client " + id, " sent " + Messages.sendPrepareFromClientToService(i+1, propNum));
+				String response = SendMessagetoServerX(Settings.serverIpList[i],Messages.sendPrepareFromClientToService(i+1, propNum));
+				
+				MessageContent parsedResponse = Messages.parse(response);
+				InternalMessageLog.WriteLog("client " + id, " received " + parsedResponse.toString());
+				
+				responseSet.add(parsedResponse);
+				if (parsedResponse.status)
+					ackCount++;
+			}
+			if (ackCount>(D/2))
+				keepTrying = false;
+			else
+			{
+				try {
+					Thread.sleep((long) (Math.random()*100));
+					propNum  = nextPropNumber(responseSet,propNum+1);
+					InternalMessageLog.WriteLog("client"+id, "FAILED AT PREPARE, RESTARTING WITH PROPNUM " +  propNum);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return responseSet;
+	}
+	
+	private long nextPropNumber(List<MessageContent> responseSet, long propNum)
+	{
+		long maxPropNumber = propNum;
+		for (int i = 0; i < responseSet.size(); i++) 
+		{
+			if (responseSet.get(i).propositionNumber > maxPropNumber)
+			{
+				maxPropNumber = responseSet.get(i).propositionNumber;
+			}
+		}
+		return maxPropNumber;
+	}
+	/**
+	 * findWinningVal
+	 * @param responseSet
+	 * @param propVal
+	 * @return
+	 */
+	public HashMap<String,String> findWinningVal(List<MessageContent> responseSet, HashMap<String,String> propVal) {
+		HashMap<String,String> winningValue = null;
+		long maxProp = Long.MIN_VALUE;
+		for (int i = 0; i < responseSet.size(); i++)
+		{
+			MessageContent current = responseSet.get(i);
+			if (current.propositionNumber>maxProp && current.vValues != null)
+			{
+				maxProp = current.propositionNumber;
+				winningValue = current.vValues;
+			}
+		}
+		if (winningValue == null || winningValue.isEmpty())
+			winningValue = propVal;
+		return winningValue;
+	}
+	/**
+	 * enhancedFindWinningVal
+	 * @param responseSet
+	 * @param propVal
+	 * @return
+	 */
+	public String enhancedFindWinningVal(List responseSet, String propVal) {
+		//have to write this
+		String winningVal = null;
+		return winningVal;
+	}
+	
+	public static void main(String[] args) 
+	{
+		Settings.init();
+		//TransactionClient client1 = new TransactionClient();
+		//client1.SendMessagetoServerX(Settings.serverIpMap.get(1),Messages.sendAcceptFromClientToService(1, 5, "a"));
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/TransactionService.java
@@ -0,0 +1,222 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import java.util.*;
+
+public class TransactionService 
+{	
+	private WriteLog log;
+	private long id;
+	private int listenPort;
+	
+	public TransactionService(long id,int listenPort)
+	{
+		this.listenPort = listenPort;
+		this.id = id;
+		new ConnectionHandler().start();
+		/*Creates a log for an entity key=1*/
+		try {
+			log = new WriteLog("log" +id);
+			System.out.println("adas");
+			log.erase();
+			log = new WriteLog("log" +id);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		/*Starts the position 0 of the log*/
+		try {
+			log.startPosition(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+		
+	}
+	class ConnectionHandler extends Thread
+	{//Listener for Server, just listens for connection
+		private ServerSocket serverSocket;
+
+		public ConnectionHandler()
+		{	//create a serversocket given port
+			try {
+				this.serverSocket = new ServerSocket(listenPort);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		public void run()
+		{
+			Socket newSocket;
+			try {
+				while (true){
+					newSocket = serverSocket.accept();//Accept socket
+					System.out.println("Client Connection accepted: "+newSocket.getInetAddress().getHostAddress());
+					//launch thread
+					MessageListener newThread = new MessageListener(newSocket);
+					newThread.start();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+		}
+	}
+	class MessageListener extends Thread
+	{//Thread that reads from client and sends a message
+		private Socket socket;
+		private PrintWriter toTransactionClient;
+		private BufferedReader fromTransactionClient;
+		private String message = null;
+		public MessageListener(Socket socket)
+		{
+			this.socket = socket;
+			try {
+				toTransactionClient = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+				fromTransactionClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		public void run()
+		{
+			try {
+				message = fromTransactionClient.readLine();
+				MessageContent parsedMessage = Messages.parse(message);
+				InternalMessageLog.WriteLog("server"+ id, " received " + parsedMessage.toString() );
+				if(parsedMessage.messageType.equals("PREPARE")) {
+					receive_prepare(parsedMessage.cid, parsedMessage.propositionNumber,toTransactionClient);
+				}
+				else if(parsedMessage.messageType.equals("ACCEPT")) {
+					receive_accept(parsedMessage.cid, parsedMessage.propositionNumber,parsedMessage.propositionValues,toTransactionClient);
+				}
+				else if(parsedMessage.messageType.equals("APPLY")) {
+					receive_apply(parsedMessage.cid, parsedMessage.propositionNumber,parsedMessage.propositionValues);
+				}
+				else
+					System.out.println("Error: Unrecognized message format received from client");
+				
+				
+				//parseMessage
+				//call corresponding Method
+				//send to client using InputStream
+				socket.close();
+				//close connections
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+	
+	public synchronized void receive_prepare(int cid, long propNum, PrintWriter toTransactionClient) {
+		boolean keepTrying = true;
+		while(keepTrying) {
+			try {
+				//position p
+				long p = log.getPosition();
+				WriteLogReadResult result = log.read(p);
+				if(propNum > result.vNextBal) {
+					if(log.checkAndWrite(p,result.vNextBal,propNum)) {
+						InternalMessageLog.WriteLog("server"+ id, " sent " + Messages.sendPrepareSuccessFromServiceToClient(cid, result.vBalloutNumber, result.values));
+						String message = Messages.sendPrepareSuccessFromServiceToClient(cid, result.vBalloutNumber, result.values);
+						toTransactionClient.println(message);
+						toTransactionClient.flush();
+						keepTrying = false;
+					}
+				}
+				else {
+					InternalMessageLog.WriteLog("server"+ id, " sent " + Messages.sendPrepareFailureFromServiceToClient(cid,result.vBalloutNumber));
+					String message = Messages.sendPrepareFailureFromServiceToClient(cid,result.vBalloutNumber);
+					toTransactionClient.println(message);
+					toTransactionClient.flush();
+					keepTrying = false;
+				}
+			}
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	/**
+	 * receive for accept message
+	 * @param cid
+	 * @param propNum
+	 * @param value
+	 */
+	public synchronized void receive_accept(int cid, long propNum, HashMap<String,String> value, PrintWriter toTransactionClient) {
+		try {
+			String message = new String();
+			System.out.println(propNum);
+			if(log.checkAndWrite(log.getPosition(), propNum, value)) {
+				//success
+				message = Messages.sendAcceptFromServiceToClient(cid,true);
+				InternalMessageLog.WriteLog("server"+ id, " sent " + Messages.sendAcceptFromServiceToClient(cid,true));
+
+			}
+			else {
+				//failure
+				message = Messages.sendAcceptFromServiceToClient(cid,false);
+				InternalMessageLog.WriteLog("server"+ id, " sent " + Messages.sendAcceptFromServiceToClient(cid,false));
+			}
+			//send message
+			toTransactionClient.println(message);
+			toTransactionClient.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * receive for apply message
+	 * @param cid
+	 * @param propNum
+	 * @param value
+	 */
+	public synchronized void receive_apply(int cid, long propNum, HashMap<String,String> value) {
+		try {
+			log.write(log.getPosition(),propNum,value);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) 
+	{
+		InternalMessageLog.init();
+		TransactionService service1 = new TransactionService(1,11111);
+		TransactionService service2 = new TransactionService(2,11112);
+		TransactionService service3 = new TransactionService(3,11113);
+	}
+
+}
